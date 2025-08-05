a67e69500ca8155b95617bfa7f08b55c0a9d5f64
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-08-05 19:23:15 +0530

    17834

--- account/models.py
+++ account/models.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True)
-    def__str__(self):
+    def __str__(self):
         return self.username
