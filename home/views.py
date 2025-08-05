b9ff66a55ed63f18d893f50a70ad53a59300c014
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-08-05 21:38:49 +0530

    17838

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIview
+from rest_framework.response import response
 
-# Create your views here.
+class MEnuAPIview(APIView):
+    def get(self, request):
+        menu = [
+            {"name": "Pasta", "description": "Creamy Alfredo pasta", "price": 250},
+            {"name": "Burger", "description": "Beef patty with cheese", "price": 180},
+            {"name": "Pizza", "description": "Margherita with fresh basil", "price": 300},
+        ]
+        return Response(menu)
+
