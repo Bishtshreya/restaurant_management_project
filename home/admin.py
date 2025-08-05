cbd4f8685933c1a382ca31526ff7225ae4f4c6d7
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-08-05 21:59:30 +0530

    17837

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Menu, Order
+admin.site.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('name','price')
+    search_fields = ('name',)
 
-# Register your models here.
+admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('menu_item', 'quantity', 'ordered_at')
+    list_filter = ('ordered_at',)
+
