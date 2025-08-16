1c20e2d94f78c283b7de2da5fad8964684c00a96
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-08-16 20:29:20 +0530

    33150

--- home/views.py
+++ home/views.py
@@ -14,7 +14,8 @@ class MenuAPIView(APIView):
         return Response(menu)
 
 def homepage(request):
-    restro_name = "Shreya Restro"
+    restro_name = settings.R
+    phone_number = 
     return render(request, 'home/index.html', {'restro_name': restro_name})
 
 def about_us(request):
