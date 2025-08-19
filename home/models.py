fb188a5d885c74d92a3086ce96d9d467948d6c83
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-08-19 21:40:12 +0530

    35519

--- home/models.py
+++ home/models.py
@@ -7,3 +7,10 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
+class MenuList(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
