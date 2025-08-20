8b6cd11bbb4567acd2ca1722c13f1bb625c658bc
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-08-20 19:43:11 +0530

    36154

--- home/models.py
+++ home/models.py
@@ -18,4 +18,18 @@ class MenuList(models.Model):
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class RestaurantLocation(models.Model):
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.address}, {self.city}, {self.state} - {self.zip_code}"
+
     
