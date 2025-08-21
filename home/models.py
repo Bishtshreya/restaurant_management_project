5c95bf8cfe79b0e1f4ba4d92ca4ae0d87225c2de
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-08-21 22:11:28 +0530

    36663

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True)
@@ -29,6 +30,8 @@ class RestaurantLocation(models.Model):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     zip_code = models.CharField(max_length=20)
+
+    opening_hours = JSONField(default=dict, blank=True)
 
     def __str__(self):
         return f"{self.address}, {self.city}, {self.state} - {self.zip_code}"
