c116679069a7a03c0a62b22fea4e87d82bed19e3
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-08-25 19:57:52 +0530

    38391

--- home/urls.py
+++ home/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path('contact/', views.contact_us),
+    path("menu/", views.menu_items, name="menu"),
     path('about/', views.about_us, name= 'about'),
     path("faq/", views.faq_view, name="faq"),
     path("", views.homepage_view, name="home"),
     path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
+    path("order/confirmation/", views.order_confirmation, name="order_confirmation"),
 ]
