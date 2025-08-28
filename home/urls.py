0cff020824eb45ca2e19ae71c1928a9c820e66e7
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-08-28 22:07:58 +0530

    39658

--- home/urls.py
+++ home/urls.py
@@ -15,7 +15,8 @@ urlpatterns = [
     path("cart/", views.view_cart, name="view_cart"),
     path("cart/add/<int:item_id>/", views.add_to_cart, name="add_to_cart"),
     path("cart/remove/<int:item_id>/", views.remove_from_cart, name="remove_from_cart"),
-
+    path("", views.home, name="home"),
+    
     #  Built-in Django auth views
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="home"), name="logout"),
