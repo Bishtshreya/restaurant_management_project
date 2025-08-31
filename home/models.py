eeeb0d51a1a0d3e1453c6501eab7a7ce7b14e506
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-08-31 21:39:30 +0530

    40584

--- home/models.py
+++ home/models.py
@@ -12,6 +12,7 @@ class MenuList(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to="menu_images/", blank=True, null=True)
+    image_alt = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
