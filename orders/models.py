1e0f0a9851aae7302c82d9136953dfbebc7eb60a
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-09-16 00:10:26 +0530

    44804

--- orders/models.py
+++ orders/models.py
@@ -22,3 +22,13 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order #{self.id} by {self.customer.username}"
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    discount = models.DecimalField(max_digits=5, decimal_places=2)  # % or amount
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.code
+        
