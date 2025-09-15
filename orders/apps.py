88911c33405bc8dc98470729a33bea39c5911548
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-09-16 00:18:48 +0530

    44805

--- orders/apps.py
+++ orders/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
-
+from django.db.utils import OperationalError, ProgrammingError
 
 class OrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'orders'
+
+    def ready(self):
+        from .models import OrderStatus
+        from . import ORDER_STATUS_CHOICES
+        try:
+            for status in ORDER_STATUS_CHOICES:
+                OrderStatus.objects.get_or_create(name=status)
+        except (OperationalError, ProgrammingError):
+            # Database not ready during first migrate
+            pass
