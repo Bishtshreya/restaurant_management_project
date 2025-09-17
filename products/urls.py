811562f5f6f62efde09fc17a60654ccf474527f6
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-09-17 22:12:58 +0530

    47127

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from .views import MenuViewSet
+from rest_framework.routers import DefaultRouter
 from .views import *
+
+router = DefaultRouter()
+router.register(r'menu', MenuViewSet, basename="menu")
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
+    path("", include(router.urls)),
 ]
