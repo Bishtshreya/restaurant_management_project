34f9237ea77ed2b4c1aee4e0cceb62fb8500ed50
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-09-17 22:28:05 +0530

    47129

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .views import MenuViewSet
 from rest_framework.routers import DefaultRouter
 from .views import *
+from .views import MenuByCategoryAPIView
 
 router = DefaultRouter()
 router.register(r'menu', MenuViewSet, basename="menu")
@@ -9,4 +10,5 @@ router.register(r'menu', MenuViewSet, basename="menu")
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path("", include(router.urls)),
+    path("menu/by-category/", MenuByCategoryAPIView.as_view(), name="menu-by-category"),
 ]
