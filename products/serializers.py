e93f54f96881976de8abb8172da8d6ddbc3bc40d
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-09-17 22:05:50 +0530

    47127

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from .models import Item
+from .models import Menu
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ["id", "name", "price"]
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Price must be greater than 0.")
+        return value
+
