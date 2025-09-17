fcb5177a58ee79758d631fad2e2bab587c47debc
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-09-17 22:10:02 +0530

    47127

--- products/views.py
+++ products/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-
+from rest_framework import viewsets, status
 from .models import Item
 from .serializers import ItemSerializer
-
-'''
-NOTE: Conside this as a reference and follow this same coding structure or format to work on you tasks
-'''
+from .models import Menu
+from .serializers import MenuSerializer
+from rest_framework.permissions import IsAdminUser
 
 # Create your views here.
 class ItemView(APIView):
@@ -24,3 +22,22 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class MenuViewSet(viewsets.ModelViewSet):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+    # Restrict update to admin users only
+    def get_permissions(self):
+        if self.action in ["update", "partial_update", "destroy", "create"]:
+            return [IsAdminUser()]
+        return super().get_permissions()
+
+    def update(self, request, *args, **kwargs):
+        try:
+            return super().update(request, *args, **kwargs)
+        except Exception as e:
+            return Response(
+                {"error": str(e)},
+                status=status.HTTP_400_BAD_REQUEST
+            )
