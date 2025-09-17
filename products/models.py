7dde514b98463767d1b6f19b52d7bec48e4c3e96
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-09-17 22:22:12 +0530

    47129

--- products/models.py
+++ products/models.py
@@ -8,3 +8,16 @@ class Item(models.Model):
 
     def __str__(self):
         return str(self.item_name)
+
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.item_name
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE, related_name="menu_items")
+
+    def __str__(self):
+        return self.name
