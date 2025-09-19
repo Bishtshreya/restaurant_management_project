7b49fd2c308234d52d82473b1dced01ff8e3d9e0
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-09-19 23:51:33 +0530

    49004

--- home/serializers.py
+++ home/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import MenuCategory
-from .models import MenuList
+from .models import MenuList, CustomUser
 
 class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +11,9 @@ class MenuListSerializer(serializers.ModelSerializer):
         model = MenuList
         fields = ["id", "name", "description", "price", "image", "available"]
         
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["username", "first_name", "last_name", "email", "phone_number"]
+        read_only_fields = ["username", "email"]
