e20d2a7b06885b426d7c21570afae2917c150b30
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-09-19 23:22:28 +0530

    49002

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import OrderHistoryView
+from .views import OrderHistoryView, OrderRetriveView
 
 urlpatterns = [
     path("history/", OrderHistoryView.as_view(), name="order-history"),
+    path("<int:pk>/", OrderRetriveView.as_view(), name="order-detail"),
 ]
     
