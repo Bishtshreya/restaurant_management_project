4d094185ad31db5396b61c69a11652a364978234
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-09-21 00:48:49 +0530

    49658

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import OrderHistoryView, OrderRetriveView
+from .views import OrderHistoryView, OrderRetriveView, OrderCancelView
 
 urlpatterns = [
     path("history/", OrderHistoryView.as_view(), name="order-history"),
     path("<int:pk>/", OrderRetriveView.as_view(), name="order-detail"),
+    path("orders/cancel/<int:pk>/", OrderCancelView.as_view, name="order-cancel")
 ]
     
