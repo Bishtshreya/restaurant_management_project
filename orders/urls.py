6a644873d358d91136d1ee2b3f6a3d6096c9ed1d
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-09-29 18:55:23 +0530

    52167

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import OrderHistoryView, OrderRetriveView, UpdateOrderStatusView
+from .views import  get_order_status, OrderHistoryView, OrderRetriveView, UpdateOrderStatusView
 
 urlpatterns = [
     path("history/", OrderHistoryView.as_view(), name="order-history"),
     path("<int:pk>/", OrderRetriveView.as_view(), name="order-detail"),
-    path("update-status/", UpdateOrderStatusView.as_view(), name="update-order-status"),
+    path("<int:order_id>/update-status/", UpdateOrderStatusView.as_view(), name="update-order-status"),
+    path("<int:order_id>/status/", get_order_status, name="order-status"),
 ]
     
