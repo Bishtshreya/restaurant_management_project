0786aee15a4e1e1ba8bf675774e21126a8b8b622
Shreya Bisht <sshreyabisht2004@gmail.com>
2025-10-07 23:03:35 +0530

    53717

--- home/models.py
+++ home/models.py
@@ -206,5 +206,14 @@ class Reservation(models.Model):
                 available_slots.append((slot_start, slot_end))
 
         return available_slots
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.related_name
                    
         
